b0b5c9f4d77bcd0e8c4792f4229d3812dbfbbabc
Joshua Kifer <joshua@moonlight.pm>
2024-03-03 16:50:31 -0700

    Scrubbed code name, modified syntax to obviate <server> tag, extension currently disabled except for syntax highlighting

--- packages/vscode/src/extension.ts
+++ packages/vscode/src/extension.ts
@@ -1,48 +1,45 @@
-import * as serverProtocol from "@volar/language-server/protocol";
-import { createLabsInfo, getTsdk } from "@volar/vscode";
-import { Uri } from "vscode";
-import { LanguageClient, TransportKind } from "vscode-languageclient/node";
+import { LanguageClient } from "vscode-languageclient/node";
 
 let client: LanguageClient;
 
 export async function activate(context) {
     console.log("ACTIVATE");
-    const module = Uri.joinPath(
-        context.extensionUri,
-        "dist",
-        "server",
-        "index.js",
-    ).fsPath;
-    client = new LanguageClient(
-        "montana",
-        "Montana Language Server",
-        {
-            run: {
-                module: module,
-                transport: TransportKind.ipc,
-                options: { execArgv: [] },
-            },
-            debug: {
-                module: module,
-                transport: TransportKind.ipc,
-                options: { execArgv: ["--nolazy", "--inspect=6009"] },
-            },
-        },
-        {
-            documentSelector: [{ language: "montana" }],
-            initializationOptions: {
-                typescript: {
-                    tsdk: (await getTsdk(context)).tsdk,
-                },
-            },
-        },
-    );
-    await client.start();
+    // const module = Uri.joinPath(
+    //     context.extensionUri,
+    //     "dist",
+    //     "server",
+    //     "index.js",
+    // ).fsPath;
+    // client = new LanguageClient(
+    //     "montana",
+    //     "Montana Language Server",
+    //     {
+    //         run: {
+    //             module: module,
+    //             transport: TransportKind.ipc,
+    //             options: { execArgv: [] },
+    //         },
+    //         debug: {
+    //             module: module,
+    //             transport: TransportKind.ipc,
+    //             options: { execArgv: ["--nolazy", "--inspect=6009"] },
+    //         },
+    //     },
+    //     {
+    //         documentSelector: [{ language: "montana" }],
+    //         initializationOptions: {
+    //             typescript: {
+    //                 tsdk: (await getTsdk(context)).tsdk,
+    //             },
+    //         },
+    //     },
+    // );
+    // await client.start();
 
-    const labs = createLabsInfo(serverProtocol);
-    labs.addLanguageClient(client);
+    // const labs = createLabsInfo(serverProtocol);
+    // labs.addLanguageClient(client);
 
-    return labs.extensionExports;
+    // return labs.extensionExports;
 }
 
 export function deactivate() {
