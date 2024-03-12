89853629ca786f3004a4ef81f6a32fba745e0e0e
Joshua Kifer <joshua@moonlight.pm>
2024-03-12 15:20:06 -0600

    Removed template from extension src, instead copying on build

--- packages/vscode/src/server/template/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./ast";
-export * from "./html";
-export * from "./parser";
-export * from "./printer";
-export * from "./scanner";
-export * from "./transform";
--- scripts/publish-extension.sh
+++ scripts/publish-extension.sh
@@ -4,6 +4,8 @@ OUT=.extension-build
 
 rm -rf $OUT
 cp -r packages/vscode $OUT
+mkdir -p $OUT/src/server/template
+cp -r packages/htmx-bun/hypermedia/template/*.ts $OUT/src/server/template
 rm -rf $OUT/dist
 bun build:grammar $OUT/dist/htmx-bun.tmLanguage.json
 esbuild --bundle --format=cjs --platform=node --minify --external:vscode --outdir=$OUT/dist $OUT/src/extension.ts
