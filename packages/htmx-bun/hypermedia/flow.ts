eab57d0d50b8c7410e96557f13d437e590802af8
Joshua Kifer <joshua@moonlight.pm>
2024-03-13 17:49:20 -0600

    Added mx-when flow tag

--- packages/htmx-bun/hypermedia/flow.ts
+++ packages/htmx-bun/hypermedia/flow.ts
@@ -1,41 +1,27 @@
 import { warn } from "~/lib/log";
 import { expressAttributeFirstValue } from "./expressor";
-import {
-    HtmlElement,
-    HtmlNode,
-    cloneHtml,
-    simpleTransformHtml,
-} from "./template";
+import { HtmlElement, HtmlNode, cloneHtml, simpleTransformHtml } from "./template";
 
 /**
  * Transforms an html ast by looking for `mx-each` attributes,
  * evaluating that attribute, and duplicating the element by
  * its array length.  If `mx-as` is present, each item will be
  * passed to the childrens scope under that name.
- * @param scope The scope to use for evaluating expressions.
  * @param node The ast to apply the each transform on.
  */
 export function transformFlowEach(node: HtmlNode) {
     simpleTransformHtml(node, (node) => {
         if (node.type === "element") {
-            const each = expressAttributeFirstValue(
-                node,
-                "mx-each",
-            ) as unknown[];
+            const each = expressAttributeFirstValue(node, "mx-each") as unknown[];
             if (!each) {
                 return node;
             }
             const as = expressAttributeFirstValue(node, "mx-as") as string;
             if (!Array.isArray(each)) {
-                warn(
-                    "flow",
-                    `Invalid 'mx-each' attribute for ${node.tag}, not an Array`,
-                );
+                warn("flow", `Invalid 'mx-each' attribute for ${node.tag}, not an Array`);
                 return;
             }
-            node.attrs = node.attrs.filter(
-                (attr) => !["mx-each", "mx-as"].includes(attr.name),
-            );
+            node.attrs = node.attrs.filter((attr) => !["mx-each", "mx-as"].includes(attr.name));
             const children = [];
             for (const item of each) {
                 const child = cloneHtml(node) as HtmlElement;
@@ -45,6 +31,24 @@ export function transformFlowEach(node: HtmlNode) {
                 children.push(child);
             }
             return children;
+        }
+        return node;
+    });
+}
+
+/**
+ * Removes a node if `mx-when` evaluates not undefined and falsy.
+ *
+ * @param {HtmlNode} node - The ast.
+ */
+export function transformFlowWhen(node: HtmlNode) {
+    simpleTransformHtml(node, (node) => {
+        if (node.type === "element") {
+            const when = expressAttributeFirstValue(node, "mx-when");
+            if (when !== undefined && !when) {
+                return [];
+            }
+            node.attrs = node.attrs.filter((attr) => !["mx-when"].includes(attr.name));
         }
         return node;
     });
