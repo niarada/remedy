a5d5c3fc550bd9883eccc11e9243b39bf02b50e0
Joshua Kifer <joshua@moonlight.pm>
2024-03-18 14:08:42 -0600

    Fixed extension publish script

--- scripts/publish-extension.sh
+++ scripts/publish-extension.sh
@@ -4,9 +4,9 @@ OUT=.extension-build
 
 rm -rf $OUT
 cp -r packages/vscode $OUT
-mkdir -p $OUT/src/server/template
-cp -r packages/server/hypermedia/template/*.ts $OUT/src/server/template
-rm -rf $OUT/dist
+mkdir -p $OUT/src/template
+cp -r packages/server/hypermedia/template/*.ts $OUT/src/template
+rm -rf $OUT/dist $OUT/src/**/*.test.ts
 bun build:grammar $OUT/dist/remedy.tmLanguage.json
 esbuild --bundle --format=cjs --platform=node --minify --external:vscode --outdir=$OUT/dist $OUT/src/extension.ts
 webpack --config scripts/webpack.config.js
