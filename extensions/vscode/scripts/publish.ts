a717e019fa9633ade5ffed9bd159f7a0b647a069
Joshua Kifer <joshua@moonlight.pm>
2024-03-26 14:24:23 -0600

    Updated workflow

--- extensions/vscode/scripts/publish.ts
+++ extensions/vscode/scripts/publish.ts
@@ -6,5 +6,5 @@ rmdirSync("dist", { recursive: true });
 await $`bun scripts/grammar.ts`;
 await $`bunx esbuild --bundle --format=cjs --platform=node --minify --external:vscode --outdir=dist src/extension.ts`;
 await $`bunx webpack --config scripts/webpack.config.js`;
-await $`bunx vsce package --no-dependencies`;
-// await $`bunx vsce publish --no-dependencies`;
+// await $`bunx vsce package --no-dependencies`;
+await $`bunx vsce publish --no-dependencies -p $VSCE_TOKEN`;
--- scripts/ci/publish.ts
+++ scripts/ci/publish.ts
@@ -1,21 +1,11 @@
 import { $ } from "bun";
-// import { simpleGit } from "simple-git";
+import getLatestVersion from "latest-version";
 
-// const currentVersion = `v${require("../package.json").version}`;
+const currentVersion = require("../package.json").version;
+const latestVersion = await getLatestVersion("@niarada/remedy");
 
-// const git = simpleGit();
+if (currentVersion === latestVersion) {
+    process.exit(0);
+}
 
-// const status = await git.status();
-
-// if (!status.isClean()) {
-//     console.log("Please commit or stash changes before tagging");
-//     process.exit(1);
-// }
-
-// console.log("Tagging version", currentVersion);
-
-// await git.addTag(currentVersion);
-// await git.pushTags();
-
-const result = await $`bun test`;
-console.log(result);
+await $`bun all:publish`;
